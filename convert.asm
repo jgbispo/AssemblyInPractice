bae9ecb5dc6b797b73b1652eb0ec4e3868d3b6ca
João Gustavo S. Bispo <jgbispo20@gmail.com>
2022-11-21 14:38:39 -0300

    remove gitignore

---
+++ convert.asm
@@ -0,0 +1,18 @@
+%include "library.inc.asm"
+
+
+section .data
+  v1 dw "105", lf, null
+
+
+section .text
+
+global _start
+
+_start: 
+
+
+string_to_int:
+
+
+int_to_string:
--- run.sh
+++
@@ -1,5 +0,0 @@
-nasm -f elf64 code.asm -o app.o 
-ld -s -o app app.o
-rm *.o
-./app
-rm app
